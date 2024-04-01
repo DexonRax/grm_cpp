89a89a52020f4841f04b83f899048c173fe28be0
Dexon Rax <dexonrax@gmail.com>
2024-04-02 01:35:40 +0200

    initial commit

---
+++ demo.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include "grm.h" 
+
+liczba_staloprzecinkowa funkcja_x2(liczba_staloprzecinkowa x){
+    zwroc x razy dwa zakoncz
+}
+
+liczba_staloprzecinkowa petla_dla(liczba_staloprzecinkowa ile){
+
+    liczba_staloprzecinkowa suma rowna zero zakoncz
+
+    dla(liczba_staloprzecinkowa i rowna zero zakoncz i mniejsze dziesiec zakoncz i dodaj_jeden){
+        suma rowna suma plus i zakoncz
+    }
+
+    zwroc suma zakoncz
+}
+
+liczba_staloprzecinkowa main(){
+    wypisz to funkcja_x2(dwa) to nastepna_linia zakoncz
+    wypisz to petla_dla(dziesiec) to nastepna_linia zakoncz
+    zwroc zero zakoncz
+}
